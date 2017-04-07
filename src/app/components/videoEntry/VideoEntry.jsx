a7e9ce08dc3679b530862e9b80fc56452c89e003
whitzhu <whitzhu@gmail.com>
2017-04-07 00:55:36 -0700

    [videoEntry] setup recording with RecordRTC
    Next: FE:setup ajax call to POST video to server BE:setup server endpoint to handle incoming POST req

--- src/app/components/videoEntry/VideoEntry.jsx
+++ src/app/components/videoEntry/VideoEntry.jsx
@@ -1,11 +1,110 @@
-import React from 'react';
+import React, { Component } from 'react';
+import RecordRTC from 'recordrtc';
+import $ from 'jquery';
 
-function VideoEntry() {
-  return (
-    <div className="container">
-      <h1>Video Entry</h1>
-    </div>
-  )
+class VideoEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      src: null,
+      recordVideo: null,
+      blob: null,
+      uploading: false,
+      uploadSuccess: null
+    }
+
+    this.getUserMedia = this.getUserMedia.bind(this);
+    this.captureUserMedia = this.captureUserMedia.bind(this);
+    this.handleVideo = this.handleVideo.bind(this);
+    this.videoError = this.videoError.bind(this);
+    this.startRecord = this.startRecord.bind(this);
+    this.stopRecord = this.stopRecord.bind(this);
+    this.uploadVideo = this.uploadVideo.bind(this);
+  }
+
+  componentDidMount(){
+    this.getUserMedia();
+  }
+
+  getUserMedia() {
+
+    navigator.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
+
+    if (navigator.getUserMedia) {
+      this.captureUserMedia( stream => this.handleVideo(stream));
+    } else {
+       console.log("getUserMedia not supported");
+    }
+  }
+
+  captureUserMedia(callback) {
+    const constraints = {
+      video: {
+        mandatory: {
+          maxWidth: 480,
+          maxHeight: 360
+        }
+      },
+      audio: true,
+    };
+
+    navigator.getUserMedia( constraints , callback , err => this.videoError(err));
+  }
+
+  handleVideo(stream) {
+    this.setState({
+      src: window.URL.createObjectURL(stream)
+    });
+  }
+
+  videoError(err) {
+    alert('Your browser cannot stream from your webcam. Please switch to Chrome or Firefox.');
+  }
+
+  uploadVideo(blob) {
+    let fd = new FormData();
+    fd.append('fname', blob);
+    $.ajax({
+      type:'POST',
+      url:'/entry/video',
+      data: fd,
+      processData: false,
+      contentType: false
+    }).done( data => console.log(data));
+  }
+
+  startRecord() {
+    this.captureUserMedia( stream => {
+      this.state.recordVideo = RecordRTC(stream, {type:'video'});
+      this.state.recordVideo.startRecording();
+    })
+
+    setTimeout( () => {
+      this.stopRecord();
+    }, 30000)
+  }
+
+  stopRecord() {
+    this.state.recordVideo.stopRecording((videoURL) => {
+      this.setState({
+         blob : this.state.recordVideo.blob,
+         src: videoURL
+       });
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Video Entry</h1>
+        <video autoPlay='true' src={this.state.src} controls></video>
+        <button onClick={this.startRecord}>Record</button>
+        <button onClick={this.stopRecord}>Stop</button>
+      </div>
+    )
+  }
 }
 
 export default VideoEntry;
