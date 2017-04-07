e6ccfc6a48de04111e4404ea1fe023c596642b34
Eugene Song <eugenessong@gmail.com>
2017-04-06 17:00:20 -0700

    Clean up code and comments

--- server/watsonAPI/watsonAPI.js
+++ server/watsonAPI/watsonAPI.js
@@ -3,8 +3,6 @@ const express = require('express');
 const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
 
 const personalityInsights = new PersonalityInsightsV3({
-  /* username, password, version, etc... */
-  // 'url': 'https://gateway.watsonplatform.net/personality-insights/api',
   'username': process.env.WATSON_ID,
   'password': process.env.WATSON_PASS,
   'version_date': '2016-10-19',
@@ -12,17 +10,6 @@ const personalityInsights = new PersonalityInsightsV3({
     'X-Watson-Learning-Opt-Out': true
   }
 });
-
-// personalityInsights.profile = personalityInsights.profile({
-//   text: 'Enter more than 100 unique words here...',
-//   consumption_preferences: true
-//   },
-//   function (err, response) {
-//     if (err)
-//       console.log('error:', err);
-//     else
-//       console.log(JSON.stringify(response, null, 2));
-// });
 
 exports.promisifiedPersonality = (text) => {
   return new Promise((resolve, reject) => {
