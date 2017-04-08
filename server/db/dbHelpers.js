b2796543c42340a97edbcaa49c4a3560952e44bb
Eugene Song <eugenessong@gmail.com>
2017-04-07 22:52:28 -0700

    Add route for userEntry

--- server/db/dbHelpers.js
+++ server/db/dbHelpers.js
@@ -1,2 +1,50 @@
-const mongoDatabase = require('./index.js');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
+const mongoDatabase = require('./index.js');
+const User = mongoDatabase.User;
+
+exports.userEntry = (userInfo) => {
+  let newUser = User({
+    name: userInfo.name,
+    user_id: userInfo.user_id,
+    password: userInfo.password
+  });
+
+  newUser.save()
+  .then(function(success) {
+    console.log(`${newUser.name} successfully added`);
+  })
+  .catch(function(err) {
+    console.log('Error occurred in userEntry to db:', err);
+  });
+};
+
+exports.logEntry = (log) => {
+  const userID = log.user_id;
+  let logEntry = {
+    entry_type: log.entry_type,
+    video_url: log.video_url,
+    audio_url: log.audio_url,
+    text: log.text,
+    watson_results: log.watson_results,
+    tags: log.tags
+  };
+
+  // User.findById(userID)
+  // .then(function(user) {
+  //   user.entries.push(logEntry);
+  // })
+  // .catch(function(err) {
+  //   console.log('Error occurred in logEntry to db:', err);
+  // });
+
+  User.findByIdAndUpdate(userID, {$push: {'entries': logEntry}}, {safe: true, upsert: false, new: true},
+    function(err, model) {
+      if (err) {
+        console.log('Error occurred in logEntry to db:', err);
+      } else {
+        console.log(`successfully added ${userID} entry:`, model);
+      }
+    });
+};
