315a2cb9aa7dff31724ec5943f29783d4027c5c2
Eugene Song <eugenessong@gmail.com>
2017-04-08 00:34:35 -0700

    Add route and functions for DB query

--- server/db/dbHelpers.js
+++ server/db/dbHelpers.js
@@ -42,3 +42,29 @@ exports.logEntry = (log) => {
       }
     });
 };
+
+
+
+exports.retrieveEntry = (query) => {
+  let targetUser = query.user || 'Bob Test';
+  //If there is no specified search, fetch user object
+  if (query.search === undefined) {
+    User.find({ user_id: targetUser })
+    .then(function(results) {
+      console.log('Results for retrieveEntry within first if:', JSON.stringify(results));
+      return JSON.stringify(results);
+    })
+    .catch(function(err) {
+      console.log('Error occurred in if statement of retrieveEntry to db:', err);
+    });
+  } else {
+    User.find({ user_id: targetUser })
+    .then(function(results) {
+      console.log('Results for retrieveEntry within else:', query.search, 'results:', JSON.stringify(results[0][query.search]));
+      return JSON.stringify(results[0][query.search]);
+    })
+    .catch(function(err) {
+      console.log('Error occurred in else statement of retrieveEntry to db:', err);
+    });
+  }
+};
