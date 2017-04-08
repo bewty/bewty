dc9207ba9b2aa933de02745f2a035563c04083bb
Eugene Song <eugenessong@gmail.com>
2017-04-07 23:43:04 -0700

    Clean code and remove console logs for database queries and routes

--- server/db/index.js
+++ server/db/index.js
@@ -7,7 +7,6 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
   console.log('Connected to mongoDB');
 });
 
