4ccc8830db0a8c57b3f866f23fc5bfda544b6396
whitzhu <whitzhu@gmail.com>
2017-04-11 16:03:09 -0700

    progress on implementing Affectiva

--- src/app/components/videoEntry/VideoEntry.jsx
+++ src/app/components/videoEntry/VideoEntry.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import RecordRTC from 'recordrtc';
 import axios from 'axios';
 import Loader from '../loader/Loader.jsx';
+import $ from 'jquery';
 
 class VideoEntry extends Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class VideoEntry extends Component {
       src: null,
       recordVideo: null,
       blob: null,
+      detector: null,
       playback: false,
       recording: false,
       uploadable: false,
@@ -27,10 +29,94 @@ class VideoEntry extends Component {
     this.stopRecord = this.stopRecord.bind(this);
     this.uploadVideo = this.uploadVideo.bind(this);
     this.getEmotion = this.getEmotion.bind(this);
+    this.affdexStart = this.affdexStart.bind(this);
+    this.affdexStop = this.affdexStop.bind(this);
+    this.affdexReset = this.affdexReset.bind(this);
+    this.log = this.log.bind(this);
   }
 
   componentDidMount() {
+    console.log('componentDidMount =======');
+
+      //Draw the detected facial feature points on the image
+    let drawFeaturePoints = (img, featurePoints)=> {
+      console.log('drawFeaturePoints=====', img, featurePoints);
+      var contxt = $('#face_video_canvas')[0].getContext('2d');
+
+      var hRatio = contxt.canvas.width / img.width;
+      var vRatio = contxt.canvas.height / img.height;
+      var ratio = Math.min(hRatio, vRatio);
+
+      contxt.strokeStyle = '#FFFFFF';
+      for (var id in featurePoints) {
+        contxt.beginPath();
+        contxt.arc(featurePoints[id].x,
+          featurePoints[id].y, 2, 0, 2 * Math.PI);
+        contxt.stroke();
+      }
+    };
+
     this.getUserMedia();
+
+    const width = 640;
+    const height = 480;
+    // const processFPS = 5;
+    const divRoot = $('#affdex_elements')[0];
+    const faceMode = affdex.FaceDetectorMode.LARGE_FACES;
+
+    //Construct a CameraDetector and specify the image width / height and face detector mode.
+    this.state.detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
+
+    //Enable detection of all Expressions, Emotions and Emojis classifiers.
+    this.state.detector.detectAllEmotions();
+    this.state.detector.detectAllExpressions();
+    this.state.detector.detectAllEmojis();
+    this.state.detector.detectAllAppearance();
+
+    console.log('=====this.state.detector', this.state.detector);
+
+    //Display canvas instead of video feed because we want to draw the feature points on it
+    $('#face_video_canvas').css('display', 'block');
+    $('#face_video').css('display', 'none');
+
+    //Add a callback to notify when camera access is allowed
+    this.state.detector.addEventListener('onWebcamConnectSuccess', function() {
+      console.log('Webcam access allowed');
+    });
+
+    //Add a callback to notify when camera access is denied
+    this.state.detector.addEventListener('onWebcamConnectFailure', function() {
+      // this.log('#logs', 'webcam denied');
+      console.log('Webcam access denied');
+    });
+
+    //Add a callback to notify when detector is stopped
+    this.state.detector.addEventListener('onStopSuccess', function() {
+      // this.log('#logs', 'The detector reports stopped');
+      console.log('======onStopSuccess======');
+      $('#results').html('');
+    });
+
+    //Add a callback to receive the results from processing an image.
+    //The faces object contains the list of the faces detected in an image.
+    //Faces object contains probabilities for all the different expressions, emotions and appearance metrics
+    this.state.detector.addEventListener('onImageResultsSuccess', function(faces, image, timestamp) {
+      console.log('=====add listener for logging emotions and results');
+      $('#results').html('');
+      console.log('#results', 'Timestamp: ' + timestamp.toFixed(2));
+      console.log('#results', 'Number of faces found: ' + faces.length);
+      if (faces.length > 0) {
+        console.log('#results', 'Appearance: ' + JSON.stringify(faces[0].appearance));
+        console.log('#results', 'Emotions: ' + JSON.stringify(faces[0].emotions, function(key, val) {
+          return val.toFixed ? Number(val.toFixed(0)) : val;
+        }));
+        console.log('#results', 'Expressions: ' + JSON.stringify(faces[0].expressions, function(key, val) {
+          return val.toFixed ? Number(val.toFixed(0)) : val;
+        }));
+        console.log('#results', 'Emoji: ' + faces[0].emojis.dominantEmoji);
+        drawFeaturePoints(image, faces[0].featurePoints);
+      }
+    });
   }
 
   getUserMedia() {
@@ -137,6 +223,32 @@ class VideoEntry extends Component {
     .catch( err => console.error('====error', err));
   }
 
+  affdexStart() {
+    if (this.state.detector && !this.state.detector.isRunning) {
+      this.state.detector.start();
+    }
+    this.log('#logs', 'Clicked the start button');
+    console.log('=====start button clicked!!');
+  }
+
+  affdexStop() {
+    console.log('=====stop button clicked!!');
+    if (this.state.detector && this.state.detector.isRunning) {
+      this.state.detector.removeEventListener();
+      this.state.detector.stop();
+    }
+  }
+
+  affdexReset() {
+    if (this.state.detector && this.state.detector.isRunning) {
+      this.state.detector.reset();
+    }
+  }
+
+  log(nodeName, msg) {
+    $(nodeName).append('<span>' + msg + '</span><br />');
+  }
+
   render() {
     return (
       <div className='container'>
@@ -154,7 +266,13 @@ class VideoEntry extends Component {
           <div className='flash-message'>
             {this.state.uploadError ? <p>Error Uploading Video, Please Try Again</p> : null }
           </div>
+          <div id='affdex_elements'> </div>
           {this.state.uploading ? <Loader /> : null }
+           <button id='start' onClick={this.affdexStart}>Affdex Start</button>
+           <button id='start' onClick={this.affdexStop}>Affdex Stop</button>
+           <button id='start' onClick={this.affdexStop}>Affdex Reset</button>
+          <div id='logs'>Logs</div>
+          <div id='results'>Results</div>
       </div>
     );
   }
