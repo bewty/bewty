0adb8691946f22270a2c9227be58df9dd84c3662
whitzhu <whitzhu@gmail.com>
2017-04-14 15:43:53 -0700

    [progress] refactoring to use redux for state management

--- src/app/components/entry/entry-snippet/EntrySnippet.jsx
+++ src/app/components/entry/entry-snippet/EntrySnippet.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-const EntrySnippet = ({entry}) => {
+const EntrySnippet = ({entry, index}) => {
   entry.text.length > 220 ? entry.text = `${entry.text.slice(0, 220)}...` : null;
+  // console.log('entry', entry);
   return (
     <div>
      {entry.length === 0 ? null :
-      <div className="entry-container">
-        <div className="entry-meta">
-          <span className="date">{entry.created_at.slice(0, 10)}</span>
-          <span className={`${entry.entry_type}-entry`}></span>
+      <Link to={`/entry/${entry._id}`}>
+        <div className="entry-container">
+          <div className="entry-meta">
+            <span className="date">{entry.created_at.slice(0, 10)}</span>
+            <span className={`${entry.entry_type}-entry`}></span>
+          </div>
+          <p>{entry.text}</p>
         </div>
-        <p>{entry.text}</p>
-      </div>
+      </Link>
     }
     </div>
   );
--- src/app/components/entry/entry-view/EntryView.jsx
+++ src/app/components/entry/entry-view/EntryView.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-export default class EntryView extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Entry View</h1>
-      </div>
-    );
-  }
-}
+const EntryView = ({match, entry}) => {
+  console.log('match.params.id', match.params.id);
+  console.log('match.params', match.params);
+  return (
+    <div>
+      <h1>Entry View</h1>
+    </div>
+  );
+};
+
+export default EntryView;
