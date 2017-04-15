0ad855ac817b731df8cc389a913b80d60da71166
Brandon Wong <b.lh.wong@gmail.com>
2017-04-15 10:37:47 -0700

    [redux] Add actions and reducers

--- src/app/reducers/AudioEntry.jsx
+++ src/app/reducers/AudioEntry.jsx
@@ -2,9 +2,23 @@ const AudioEntry = (state = {}, action) => {
   switch (action.type) {
   case 'SET_SOURCE_URL':
     return {
-
+      url: action.url
     };
   default:
     return state;
   }
 };
+
+const AudioEntries = (state = [], action) => {
+  switch (action.type) {
+  case 'SET_SOURCE_URL':
+    return [
+      ...state,
+      AudioEntry(undefined, action)
+    ];
+  default:
+    return state;
+  }
+};
+
+export default AudioEntries;
