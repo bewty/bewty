2d62eb68c7263766f3e369cb74cfa79086e81625
Brandon Wong <b.lh.wong@gmail.com>
2017-04-14 19:27:06 -0700

    [Refactor] Clean up code and take out silly messages

--- src/app/UserProfile.jsx
+++ src/app/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Loader from './components/loader/Loader.jsx'
+import Loader from './components/loader/Loader.jsx';
 
 
 export default class UserProfile extends React.Component {
@@ -7,38 +7,35 @@ export default class UserProfile extends React.Component {
     super (props);
   }
   componentDidMount() {
-    console.log('this da lock we passin down', this.props.lock)
     var appearanceOpts = {
       autoclose: true
     };
-    this.props.lock.sms(appearanceOpts,function (error, profile, id_token, access_token, state, refresh_token) {
-    if (error) {
-      console.log("Error inside componentDidMount under getProfile", err);
-      return;
-    }
-    else {
-      console.log(this.props, 'da props')
-      this.props.getProfile(profile, id_token)
-    }
-    }.bind(this))
+    this.props.lock.sms(appearanceOpts, function (error, profile, id_token, access_token, state, refresh_token) {
+      if (error) {
+        // console.log("Error inside componentDidMount under getProfile", err);
+        // TODO: HANDLE ERROR
+        return;
+      } else {
+        this.props.getProfile(profile, id_token);
+      }
+    }.bind(this));
   }
 
   render () {
-    if(this.props.state.profile) {
+    if (this.props.state.profile) {
       return (
         <div>
           <img className="profilePic" src={this.props.state.profile.picture} />
-          <h2 className="profileName"> Welcome! </h2>
-          <button onClick={this.props.logOut.bind(this)}> Logout </button>
+          <h2 className="profileName">Welcome!</h2>
+          <button onClick={this.props.logOut.bind(this)}>Logout</button>
         </div>
-      )
+      );
     } else {
       return (
         <div>
           <h1> Please login </h1>
           <Loader />
         </div>
-
       );
     }
   }
