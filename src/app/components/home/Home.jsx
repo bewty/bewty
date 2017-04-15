777eefdc0411c7cafc361c79b650e9df7dc34f9d
Brandon Wong <b.lh.wong@gmail.com>
2017-04-14 18:56:23 -0700

    [Refactor] Change component declaration for eslint

--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Home() {
+const Home = () => {
   return (
     <div className="container home">
       <h1>Home</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
