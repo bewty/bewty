d20eade31d572e41e923ac1bd9048eb3787abd6a
whitzhu <whitzhu@gmail.com>
2017-04-14 21:40:02 -0700

    [Redux] Setup redux for Entry components

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,23 +1,18 @@
 export const FETCH_ENTRY = 'FETCH_ENTRY';
-export const ACTIVE_ENTRY = 'ACTIVE_ENTRY';
+export const ENTRY_SELECTED = 'ENTRY_SELECTED';
 import axios from 'axios';
 
-export function fetchEntry() {
-  let entries;
-  axios.post('/db/retrieveEntry')
-  .then( result => {
-    entries = result.data;
-  });
-  console.log('===fetchEntry===== invoked', entries);
+export function fetchEntry(entries) {
+  console.log('action=====fetchEntry===entries', entries);
   return {
     type: FETCH_ENTRY,
     payload: entries
   };
 }
 
-export function clickEntry(index) {
+export function selectEntry(entry) {
   return {
-    type: ACTIVE_ENTRY,
-    index: index
+    type: ENTRY_SELECTED,
+    payload: entry
   };
 }
