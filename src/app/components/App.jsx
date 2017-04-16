debfb4c70cf5b55eafaaa21b287acadf816430e6
Brandon Wong <b.lh.wong@gmail.com>
2017-04-15 19:34:40 -0700

    [Refactor] Hide auth0 keys

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import UserProfile from '../UserProfile.jsx';
 import axios from 'axios';
+import config from '../../../config.js';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -43,7 +44,7 @@ export default class App extends React.Component {
   }
 
   createLock() {
-    this.lock = new Auth0LockPasswordless('8Xf5mRZcDDcMo0Dkl7OvMLP7ai9jULsn', 'tungnh91.auth0.com');
+    this.lock = new Auth0LockPasswordless(config.auth0_clientId, config.auth0_domain);
     this.getIdToken();
   }
 
