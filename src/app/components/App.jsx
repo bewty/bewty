675c582b3ff2ffe14fe4f255974f5469b3fad394
Tim Nguyen <tungnh91@gmail.com>
2017-04-15 19:24:37 -0700

    exported auth0 keys

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import UserProfile from '../UserProfile.jsx';
+import config from '../../../config.js'
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -17,7 +18,7 @@ export default class App extends React.Component {
     this.getProfile();
   }
   createLock() {
-    this.lock = new Auth0LockPasswordless('8Xf5mRZcDDcMo0Dkl7OvMLP7ai9jULsn', 'tungnh91.auth0.com');
+    this.lock = new Auth0LockPasswordless(config.auth0_clientId, config.auth0_domain);
     this.getIdToken();
   }
 
