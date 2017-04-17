39de7407631089798061778f132232befcec066c
whitzhu <whitzhu@gmail.com>
2017-04-17 09:57:22 -0700

    [Feature] Video graph for video-entry to display avg_data

--- src/app/containers/entry-view/EntryView.jsx
+++ src/app/containers/entry-view/EntryView.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Daily from '../../components/results/daily-chart/Daily.jsx';
+import VideoChart from '../../components/results/video-chart/VideoChart.jsx';
 import MediaPlayer from '../../components/mediaplayer/MediaPlayer.jsx';
 import EntryTextDisplay from '../../components/entry-text-display/EntryTextDisplay.jsx';
 
@@ -17,7 +18,7 @@ class EntryView extends Component {
         {entrySelected === null ? null :
           <div>
             <div className="chart-entry">
-              <Daily data={entrySelected.watson_results}/>
+            {entrySelected.entry_type === 'video' ?  <VideoChart data={entrySelected.video.avg_data}/> : <Daily data={entrySelected.watson_results}/> }
             </div>
             <EntryTextDisplay entry={entrySelected}/>
             {entrySelected.entry_type === 'text' ? null : <MediaPlayer mediaSrc={fetchMedia} mediaType={entrySelected.entry_type}/>}
