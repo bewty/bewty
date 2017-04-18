cc56e60175e099cfa22608fc9abaaabf7cb9e3dd
Brandon Wong <b.lh.wong@gmail.com>
2017-04-17 20:19:03 -0700

    [Redux] Change to object assign to not overwrite states

--- src/app/reducers/reducer_audio.jsx
+++ src/app/reducers/reducer_audio.jsx
@@ -1,29 +1,29 @@
 export default (state = {}, action) => {
   switch (action.type) {
   case 'SET_SOURCE_URL':
-    return {
+    return Object.assign({}, state, {
       src: action.url
-    };
+    });
   case 'STOP_RECORD_AND_SET':
-    return {
+    return Object.assign({}, state, {
       blob: action.blob,
       stop: action.stop,
       src: action.url
-    };
+    });
   case 'SET_START_STATE':
-    return {
+    return Object.assign({}, state, {
       start: action.start
-    };
+    });
   case 'RESET_START_STOP':
-    return {
+    return Object.assign({}, state, {
       start: action.start,
       stop: action.stop
-    };
+    });
   case 'SET_START_AND_TRANSCRIPT':
-    return {
+    return Object.assign({}, state, {
       start: action.start,
       transcript: action.transcript
-    };
+    });
   default:
     return state;
   }
