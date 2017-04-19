09c8a1286c2a92aca17865795e6daceeed4164cc
Brandon Wong <b.lh.wong@gmail.com>
2017-04-18 22:11:28 -0700

    [Refactor] Remove unecessary packages and comments

--- src/app/components/drawer/Drawer.jsx
+++ src/app/components/drawer/Drawer.jsx
@@ -1,47 +1,12 @@
 import React from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
 import Divider from 'material-ui/Divider';
-import FlatButton from 'material-ui/FlatButton';
-import Toggle from 'material-ui/Toggle';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-// class Login extends React.Component {
-//   static muiName = 'FlatButton';
-
-//   render() {
-//     return (
-//       <FlatButton {...this.props} label="Login" />
-//     );
-//   }
-// }
-// const Logged = (props) => (
-//   <IconMenu
-//     {...props}
-//     iconButtonElement={
-//       <IconButton><MoreVertIcon /></IconButton>
-//     }
-//     targetOrigin={{horizontal: 'right', vertical: 'top'}}
-//     anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-//   >
-//     <MenuItem primaryText="Refresh" />
-//     <MenuItem primaryText="Help" />
-//     <MenuItem primaryText="Sign out" />
-//   </IconMenu>
-// );
-
-// Logged.muiName = 'IconMenu';
-
 export default class AppDrawer extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {open: false};
