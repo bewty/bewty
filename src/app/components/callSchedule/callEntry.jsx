fc32c47b520813c34ebefc5d4c52f3067d32f5ff
Brandon Wong <b.lh.wong@gmail.com>
2017-04-23 23:52:40 -0700

    [Style] Convert to list items instead of drop menu

--- src/app/components/callSchedule/callEntry.jsx
+++ src/app/components/callSchedule/callEntry.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import EntryTextDisplay from '../entry-text-display/EntryTextDisplay';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
+import {List, ListItem} from 'material-ui/List';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class CallEntry extends React.Component {
@@ -9,29 +8,36 @@ export default class CallEntry extends React.Component {
     super(props);
   }
   render() {
-    return (
-      <div>
-        <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
-        <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
-        {this.props.call.responses.map((response) => {
-          if (response.text) {
-            return <EntryTextDisplay entry={response} type={'snippet'}/>;
-          }
-        })}
-      </div>
-    );
     // return (
     //   <div>
-    //     <MuiThemeProvider>
-    //       <DropDownMenu>
-    //         <MenuItem>
-    //           <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
-    //           <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
-    //         </MenuItem>
-    //       </DropDownMenu>
-    //     </MuiThemeProvider>
+    //     <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
+    //     <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
+    //     {this.props.call.responses.map((response) => {
+    //       if (response.text) {
+    //         return <EntryTextDisplay entry={response} type={'snippet'}/>;
+    //       }
+    //     })}
     //   </div>
-
     // );
+    return (
+      <div>
+        <MuiThemeProvider>
+
+            <ListItem
+              nestedItems={this.props.call.responses.map((response) => {
+                if (response.text) {
+                  return <EntryTextDisplay entry={response} type={'snippet'}/>;
+                }
+              })}
+            >
+              <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
+              <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
+
+            </ListItem>
+
+        </MuiThemeProvider>
+      </div>
+
+    );
   }
 }
--- src/app/components/callSchedule/callEntryList.jsx
+++ src/app/components/callSchedule/callEntryList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import CallEntry from './CallEntry.jsx';
+import {List, ListItem} from 'material-ui/List';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Subheader from 'material-ui/Subheader';
 
 export default class CallEntryList extends React.Component {
   constructor(props) {
@@ -11,19 +14,44 @@ export default class CallEntryList extends React.Component {
 
   render() {
     if (this.props.call_data.length > 0) {
-      return (
-        <div>
-          <h1>Call Entry List</h1>
-          {this.props.call_data.map((call) => {
-            return <CallEntry call={call} />;
-          })}
-        </div>
+    //   return (
+    //     <div>
+    //       <h1>Call Entry List</h1>
+    //       {this.props.call_data.map((call) => {
+    //         return <CallEntry call={call} />;
+    //       })}
+    //     </div>
+    // );
+    return (
+      <div>
+        <MuiThemeProvider>
+          <List>
+            <Subheader>Call Entry List</Subheader>
+
+            {this.props.call_data.map((call) => {
+              return <CallEntry call={call} />;
+            })}
+          </List>
+        </MuiThemeProvider>
+      </div>
     );
     } else {
+      // return (
+      //   <div>
+      //     <h1>Call Entry List</h1>
+      //     No entries yet
+      //   </div>
+      // );
       return (
         <div>
-          <h1>Call Entry List</h1>
-          No entries yet
+          <MuiThemeProvider>
+            <List>
+              <Subheader>Call Entry List</Subheader>
+              <ListItem
+                primaryText="No entries yet"
+              />
+            </List>
+          </MuiThemeProvider>
         </div>
       );
     }
