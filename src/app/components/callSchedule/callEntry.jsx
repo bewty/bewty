1913873e3b89eeee2a3c17f1592e81fc4c2deb08
Brandon Wong <b.lh.wong@gmail.com>
2017-04-23 18:54:24 -0700

    [Style] Attempt convert callentries to mui list

--- src/app/components/callSchedule/callEntry.jsx
+++ src/app/components/callSchedule/callEntry.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import EntryTextDisplay from '../entry-text-display/EntryTextDisplay';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class CallEntry extends React.Component {
   constructor(props) {
@@ -17,5 +20,18 @@ export default class CallEntry extends React.Component {
         })}
       </div>
     );
+    // return (
+    //   <div>
+    //     <MuiThemeProvider>
+    //       <DropDownMenu>
+    //         <MenuItem>
+    //           <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
+    //           <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
+    //         </MenuItem>
+    //       </DropDownMenu>
+    //     </MuiThemeProvider>
+    //   </div>
+
+    // );
   }
 }
