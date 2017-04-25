e80e0b959439031f76919695a995e66020ceaea6
Brandon Wong <b.lh.wong@gmail.com>
2017-04-24 23:25:52 -0700

    [Refactor] Clean up code

--- src/app/containers/call-schedule/CallEntry.jsx
+++ src/app/containers/call-schedule/CallEntry.jsx
@@ -5,30 +5,18 @@ import EntryTextDisplay from '../entry-text-display/EntryTextDisplay';
 import {List, ListItem} from 'material-ui/List';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import moment from 'moment';
-// import { selectEntry } from '../../actions/index';
 import { selectEntry } from '../../actions/index';
 
 class CallEntry extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
-    // return (
-    //   <div>
-    //     <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
-    //     <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
-    //     {this.props.call.responses.map((response) => {
-    //       if (response.text) {
-    //         return <EntryTextDisplay entry={response} type={'snippet'}/>;
-    //       }
-    //     })}
-    //   </div>
-    // );
     console.log(this.props);
     return (
       <div className="entry-container call-entry">
         <MuiThemeProvider>
-
             <ListItem
               innerDivStyle={{padding: '0'}}
               nestedListStyle={{padding: '0'}}
@@ -45,7 +33,6 @@ class CallEntry extends React.Component {
                     >
                       <EntryTextDisplay entry={response} type={'snippet'} index={index}/>
                     </div>
-
                   );
                 }
               })}
@@ -59,62 +46,12 @@ class CallEntry extends React.Component {
                   <span className="time">{moment(this.props.call.date_set).format('h:mm a')}</span>
                 </div>
               </div>
-
             </ListItem>
-
         </MuiThemeProvider>
       </div>
-
     );
-    // return (
-    //   <div>
-    //     <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
-    //     <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
-    //     {this.props.call.responses.map((response) => {
-    //       if (response.text) {
-    //         return <EntryTextDisplay entry={response} type={'snippet'}/>;
-    //       }
-    //     })}
-    //   </div>
-    // );
-    // return (
-    //   <div>
-    //     <MuiThemeProvider>
-
-    //         <ListItem
-    //           innerDivStyle={{padding: '0'}}
-    //           style={{fontFamily: 'Lato, sans-serif'}}
-    //           primaryTogglesNestedList={true}
-    //           nestedItems={this.props.call.responses.map((response) => {
-    //             if (response.text) {
-    //               return <EntryTextDisplay entry={response} type={'snippet'}/>;
-    //             }
-    //           })}
-    //         >
-    //           <div className="header-box">
-    //             <h3 className="question">{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h3>
-    //             <div className="entry-meta">
-    //               <span className="date">{moment(this.props.call.date_set).format('MM-DD-YYYY')}</span>
-    //             </div>
-    //             <div className="time-container">
-    //               <span className="time">{moment(this.props.call.date_set).format('h:mm a')}</span>
-    //             </div>
-    //           </div>
-
-    //         </ListItem>
-
-    //     </MuiThemeProvider>
-    //   </div>
-
-    // );
   }
 }
-
-
-              /*
-              <h2>{this.props.call.question[this.props.call.question.length - 1] === '?' ? this.props.call.question : this.props.call.question + '?'}</h2>
-              <h3>Started on: {this.props.call.date_set.slice(0, 10)}</h3> <h4> Calltime: {this.props.call.call_time.slice(0, 2) + ':' + this.props.call.call_time.slice(2)}</h4>
-              */
 
 function mapStateToProps(state) {
   return {
