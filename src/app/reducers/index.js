c34aef5b43b2afe9aadfa732258adf193f0c9b77
Brandon Wong <b.lh.wong@gmail.com>
2017-04-27 09:43:34 -0700

    [Refactor] Remove audio reducer

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import FetchEntry from './reducer_fetch_entry.js';
 import FetchMedia from './reducer_fetch_media.js';
 import EntrySelected from './reducer_entry_selected.js';
-import AudioReducer from './reducer_audio.jsx';
 
 const rootReducer = combineReducers({
   entries: FetchEntry,
   entrySelected: EntrySelected,
-  fetchMedia: FetchMedia,
-  audio: AudioReducer
+  fetchMedia: FetchMedia
 });
 
 export default rootReducer;
