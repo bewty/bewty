04efd3672582dda7c5bd50ab36b4dda9f39fe7c0
Brandon Wong <b.lh.wong@gmail.com>
2017-04-27 00:21:59 -0700

    [Refactor] White spaces, take out console logs, error handle todo

--- server/twilioAPI/twilioAPI.js
+++ server/twilioAPI/twilioAPI.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-
 exports.dialNumbers = (number, message) => {
   client.calls.create({
     url: `https://handler.twilio.com/twiml/EHbfb96ad7bafedf5e02460070a5bae8e7?message=${message}`,
@@ -11,11 +10,11 @@ exports.dialNumbers = (number, message) => {
     transcribe: true
   }, (err, call) => {
     if (err) {
-      console.log('Error occurred in twilioAPI clientCall:', err);
-    } else {
-      console.log('TwilioAPI making call to:', call.to);
+      // console.log('Error occurred in twilioAPI clientCall:', err);
+      // TODO: HANDLE ERROR
     }
+    // else {
+      // console.log('Making call to:', call.to, 'Twilio SID:', call.sid, 'uri:', call.uri);
+    // }
   });
 };
-
-
