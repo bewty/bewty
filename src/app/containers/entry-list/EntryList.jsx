23b0128549222eccdafe4959646cef74b1bffbcf
Brandon Wong <b.lh.wong@gmail.com>
2017-04-27 09:41:46 -0700

    [Refactor] Remove console logs and add error handle reminder

--- src/app/containers/entry-list/EntryList.jsx
+++ src/app/containers/entry-list/EntryList.jsx
@@ -22,8 +22,9 @@ class EntryList extends Component {
     .then((result) => {
       this.props.fetchEntry(result.data);
     })
-    .catch((err) => {
-      console.error('Fetching Entry Error', err.message);
+    .catch( err => {
+      // console.error('Fetching Entry Error', err.message)
+      // TODO: HANDLE ERROR
     });
   }
 
@@ -33,8 +34,9 @@ class EntryList extends Component {
       .then( result => {
         this.props.fetchMedia(result.data);
       })
-      .catch((err) => {
-        console.error('Fetching Media Error');
+      .catch( err => {
+        // console.error('Fetching Media Error');
+        // TODO: HANDLE ERROR
       });
     }
   }
